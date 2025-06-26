f3a7a6061de36f29cbd5d057f4bbf4d938c25030
fedorovskyi <anton.fedorovsky@gmail.com>
2025-06-26 17:23:23 +0200

    added vite-project

---
+++ projects/vite-project/src/App.tsx
@@ -0,0 +1,17 @@
+import { PigmentCard } from '@fedorovskyi/ui-kit';
+import { ThemeSwitcher } from './components/theme-switcher';
+import { InternalCard } from './components/internal-card';
+
+
+function App() {
+
+  return (
+    <>
+        <ThemeSwitcher />
+        <PigmentCard />
+        <InternalCard />
+    </>
+  )
+}
+
+export default App
