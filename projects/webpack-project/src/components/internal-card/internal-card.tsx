0a71f26f168cbea63a059082cc0d8d658f337ffc
fedorovskyi <anton.fedorovsky@gmail.com>
2025-06-26 17:01:28 +0200

    added webpack-project

---
+++ projects/webpack-project/src/components/internal-card/internal-card.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { styled } from '@linaria/react';
+import { tokens } from '@fedorovskyi/theme';
+
+const Title = styled.h1`
+  color: ${tokens.colors.textNeutral100};
+    background-color: ${tokens.colors.neutral100};
+`;
+
+export const InternalCard = () => {
+  return (
+    <div>
+      <Title>Internal Card</Title>
+    </div>
+  );
+};
---
+++ projects/webpack-project/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          {
+            loader: '@wyw-in-js/webpack-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+};
