429fb3a0a3c05b7d8f4952e86a4a1f063e142d81
fedorovskyi <anton.fedorovsky@gmail.com>
2025-06-29 19:36:31 +0200

    internal card

--- projects/webpack-project/src/components/internal-card/internal-card.tsx
+++ projects/webpack-project/src/components/internal-card/internal-card.tsx
@@ -4,13 +4,36 @@ import { tokens } from '@fedorovskyi/theme';
 
 const Title = styled.h1`
   color: ${tokens.colors.textNeutral100};
-    background-color: ${tokens.colors.neutral100};
+  background-color: ${tokens.colors.neutral100};
+`;
+
+const commonStyles = `
+  background-color: #ccc;
+`;
+
+const Card = styled.div<{ color?: 'red' | 'green' | 'blue' }>`
+  ${commonStyles};
+  border-width: 5px;
+  border-style: solid;
+
+  border-color: ${({ color }) => {
+    switch (color) {
+      case 'red':
+        return `#f00`;
+      case 'green':
+        return `#0f0`;
+      case 'blue':
+        return `#00f`;
+      default:
+        return `#000`;
+    }
+  }};
 `;
 
 export const InternalCard = () => {
   return (
-    <div>
+    <Card color="red">
       <Title>Internal Card</Title>
-    </div>
+    </Card>
   );
 };
