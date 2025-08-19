e717990ccf3c0cd0e5714aaef62084237af72bcc
a.fedorovskyi <anton.fedorovsky@gmail.com>
2025-08-19 15:10:19 +0200

    added size

--- projects/vite-project/src/components/internal-card/internal-card.tsx
+++ projects/vite-project/src/components/internal-card/internal-card.tsx
@@ -1,9 +1,14 @@
 import { styled } from '@linaria/react';
 import { tokens } from '@fedorovskyi/theme';
 
-const Title = styled.h1`
+interface TitleProps {
+  $size?: number;
+}
+
+const Title = styled.h1<TitleProps>`
   color: ${tokens.colors.textNeutral100};
   background-color: ${tokens.colors.neutral100};
+  font-size: ${({ $size }) => $size ? `${$size}px` : '24px'};
 `;
 
 export const InternalCard = () => {
