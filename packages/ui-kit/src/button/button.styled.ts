ae2c32eb8e8b2b668eb8549e72a1700942e5600b
a.fedorovskyi <anton.fedorovsky@gmail.com>
2025-08-22 12:34:27 +0200

    added button

--- packages/ui-kit/src/button/button.styled.ts
+++ packages/ui-kit/src/button/button.styled.ts
@@ -63,6 +63,178 @@ const variantSize: Record<
   },
 };
 
+const variantColor: Record<
+  ButtonColor,
+  {
+    normal: {
+      color: string;
+      backgroundColor: string;
+      backgroundImage: string;
+    };
+    hover: {
+      color: string;
+      backgroundColor: string;
+      backgroundImage: string;
+    };
+    focus: {
+      color: string;
+      backgroundColor: string;
+      backgroundImage: string;
+    };
+    active: {
+      color: string;
+      backgroundColor: string;
+      backgroundImage: string;
+    };
+    disabled: {
+      color: string;
+      backgroundColor: string;
+      backgroundImage: string;
+    };
+  }
+> = {
+  primary: {
+    normal: {
+      color: tokens.components.buttons.primary.normal.text,
+      backgroundColor: tokens.components.buttons.primary.normal.backgroundColor,
+      backgroundImage: tokens.components.buttons.primary.normal.backgroundImage,
+    },
+    hover: {
+      color: tokens.components.buttons.primary.hover.text,
+      backgroundColor: tokens.components.buttons.primary.hover.backgroundColor,
+      backgroundImage: tokens.components.buttons.primary.hover.backgroundImage,
+    },
+    focus: {
+      color: tokens.components.buttons.primary.hover.text,
+      backgroundColor: tokens.components.buttons.primary.hover.backgroundColor,
+      backgroundImage: tokens.components.buttons.primary.hover.backgroundImage,
+    },
+    active: {
+      color: tokens.components.buttons.primary.active.text,
+      backgroundColor: tokens.components.buttons.primary.active.backgroundColor,
+      backgroundImage: tokens.components.buttons.primary.active.backgroundImage,
+    },
+    disabled: {
+      color: tokens.components.buttons.primary.disabled.text,
+      backgroundColor:
+        tokens.components.buttons.primary.disabled.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.primary.disabled.backgroundImage,
+    },
+  },
+  secondary: {
+    normal: {
+      color: tokens.components.buttons.secondary.normal.text,
+      backgroundColor:
+        tokens.components.buttons.secondary.normal.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.secondary.normal.backgroundImage,
+    },
+    hover: {
+      color: tokens.components.buttons.secondary.hover.text,
+      backgroundColor:
+        tokens.components.buttons.secondary.hover.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.secondary.hover.backgroundImage,
+    },
+    focus: {
+      color: tokens.components.buttons.secondary.hover.text,
+      backgroundColor:
+        tokens.components.buttons.secondary.hover.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.secondary.hover.backgroundImage,
+    },
+    active: {
+      color: tokens.components.buttons.secondary.active.text,
+      backgroundColor:
+        tokens.components.buttons.secondary.active.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.secondary.active.backgroundImage,
+    },
+    disabled: {
+      color: tokens.components.buttons.secondary.disabled.text,
+      backgroundColor:
+        tokens.components.buttons.secondary.disabled.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.secondary.disabled.backgroundImage,
+    },
+  },
+  system_regular: {
+    normal: {
+      color: tokens.components.buttons.system_regular.normal.text,
+      backgroundColor:
+        tokens.components.buttons.system_regular.normal.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_regular.normal.backgroundImage,
+    },
+    hover: {
+      color: tokens.components.buttons.system_regular.hover.text,
+      backgroundColor:
+        tokens.components.buttons.system_regular.hover.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_regular.hover.backgroundImage,
+    },
+    focus: {
+      color: tokens.components.buttons.system_regular.hover.text,
+      backgroundColor:
+        tokens.components.buttons.system_regular.hover.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_regular.hover.backgroundImage,
+    },
+    active: {
+      color: tokens.components.buttons.system_regular.active.text,
+      backgroundColor:
+        tokens.components.buttons.system_regular.active.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_regular.active.backgroundImage,
+    },
+    disabled: {
+      color: tokens.components.buttons.system_regular.disabled.text,
+      backgroundColor:
+        tokens.components.buttons.system_regular.disabled.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_regular.disabled.backgroundImage,
+    },
+  },
+  system_light: {
+    normal: {
+      color: tokens.components.buttons.system_light.normal.text,
+      backgroundColor:
+        tokens.components.buttons.system_light.normal.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_light.normal.backgroundImage,
+    },
+    hover: {
+      color: tokens.components.buttons.system_light.hover.text,
+      backgroundColor:
+        tokens.components.buttons.system_light.hover.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_light.hover.backgroundImage,
+    },
+    focus: {
+      color: tokens.components.buttons.system_light.hover.text,
+      backgroundColor:
+        tokens.components.buttons.system_light.hover.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_light.hover.backgroundImage,
+    },
+    active: {
+      color: tokens.components.buttons.system_light.active.text,
+      backgroundColor:
+        tokens.components.buttons.system_light.active.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_light.active.backgroundImage,
+    },
+    disabled: {
+      color: tokens.components.buttons.system_light.disabled.text,
+      backgroundColor:
+        tokens.components.buttons.system_light.disabled.backgroundColor,
+      backgroundImage:
+        tokens.components.buttons.system_light.disabled.backgroundImage,
+    },
+  },
+};
+
 const getPadding = ($size: ButtonSize, $fluid?: boolean) => {
   if ($fluid) {
     return '0';
@@ -72,7 +244,7 @@ const getPadding = ($size: ButtonSize, $fluid?: boolean) => {
 
 export const Button = styled.button<{
   $size: ButtonSize;
-  $color?: ButtonColor;
+  $color: ButtonColor;
   $shadow?: boolean;
   $fluid?: boolean;
   disabled?: boolean;
@@ -80,6 +252,7 @@ export const Button = styled.button<{
   border: none;
   cursor: pointer;
   position: relative;
+  outline: none;
 
   border-radius: ${({ $size }) => variantSize[$size].borderRadius};
   padding: ${({ $size, $fluid }) => getPadding($size, $fluid)};
@@ -89,6 +262,48 @@ export const Button = styled.button<{
     $shadow ? '0 0.5rem 1rem -0.5rem rgba(0, 0, 0, 0.3)' : 'none'};
 
   width: ${({ $fluid }) => ($fluid ? '100%' : 'auto')};
+
+  color: ${({ $color }) => variantColor[$color].normal.color};
+
+  background-color: ${({ $color }) =>
+    variantColor[$color].normal.backgroundColor};
+
+  background-image: ${({ $color }) =>
+    variantColor[$color].normal.backgroundImage};
+
+  &:hover {
+    color: ${({ $color }) => variantColor[$color].hover.color};
+    background-color: ${({ $color }) =>
+      variantColor[$color].hover.backgroundColor};
+    background-image: ${({ $color }) =>
+      variantColor[$color].hover.backgroundImage};
+  }
+
+  &:focus {
+    color: ${({ $color }) => variantColor[$color].focus.color};
+    background-color: ${({ $color }) =>
+      variantColor[$color].focus.backgroundColor};
+    background-image: ${({ $color }) =>
+      variantColor[$color].focus.backgroundImage};
+  }
+
+  &:active {
+    color: ${({ $color }) => variantColor[$color].active.color};
+    background-color: ${({ $color }) =>
+      variantColor[$color].active.backgroundColor};
+    background-image: ${({ $color }) =>
+      variantColor[$color].active.backgroundImage};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    box-shadow: none;
+    color: ${({ $color }) => variantColor[$color].disabled.color};
+    background-color: ${({ $color }) =>
+      variantColor[$color].disabled.backgroundColor};
+    background-image: ${({ $color }) =>
+      variantColor[$color].disabled.backgroundImage};
+  }
 `;
 
 export const TextContent = styled.div<{
